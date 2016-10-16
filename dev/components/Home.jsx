8faecd9191d5df30984d8098e666ca966f41a760
Freddie Lindsey <freddie.a.lindsey@gmail.com>
2016-10-16 16:51:08 +0100

    Add Home component

---
+++ dev/components/Home.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class Home extends Component {
+
+  static displayName = 'Home';
+
+  render() {
+    return (
+      <div>
+        Home page changes!
+      </div>
+    );
+  }
+
+}
+
+export default Home
