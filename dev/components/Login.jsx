947fbf272487da1eacf16bd609db1ab3b2a2989c
Gregoire <Gregoire@Gregoires-MacBook-Air.local>
2016-10-18 10:51:44 +0100

    Added fiels

--- dev/components/Login.jsx
+++ dev/components/Login.jsx
@@ -11,6 +11,35 @@ class Login extends Component {
         <div>
           <div className="form">
               <Link to='/'>Home</Link> <Link to='/signup'>Sign Up</Link>
+
+            <div className="tab-content">
+              <div id="login">
+                <h1>Welcome Back!</h1>
+
+                <form action="/" method="post">
+
+                  <div className="field-wrap">
+                  <label>
+                    Email Address<span className="req">*</span>
+                  </label>
+                  <input type="email"required autoComplete="off"/>
+                </div>
+
+                <div className="field-wrap">
+                  <label>
+                    Password<span className="req">*</span>
+                  </label>
+                  <input type="password"required autoComplete="off"/>
+                </div>
+
+                <p className="forgot"><a href="#">Forgot Password?</a></p>
+
+                <button className="button button-block">Log In</button>
+
+                </form>
+
+              </div>
+            </div>
           </div>
         </div>
     );
--- dev/components/SignUp.jsx
+++ dev/components/SignUp.jsx
@@ -17,6 +17,44 @@ class SignUp extends Component {
     return (
       <div>
         <Link to='/'>Home</Link> <Link to='/login'>Login</Link>
+
+        <h1>Sign Up for Free</h1>
+
+        <form action="/" method="post">
+
+        <div className="top-row">
+          <div className="field-wrap" >
+            <label>
+              First Name<span className="req">*</span>
+            </label>
+            <input type="text" required autoComplete="off" />
+          </div>
+
+          <div className="field-wrap">
+            <label>
+              Last Name<span className="req">*</span>
+            </label>
+            <input type="text" required autoComplete="off"/>
+          </div>
+        </div>
+
+        <div className="field-wrap">
+          <label>
+            Email Address<span className="req">*</span>
+          </label>
+          <input type="email"required autoComplete="off"/>
+        </div>
+
+        <div className="field-wrap">
+          <label>
+            Set A Password<span className="req">*</span>
+          </label>
+          <input type="password" srequired autoComplete="off"/>
+        </div>
+
+        <button type="submit" className="button button-block">Get Started</button>
+
+        </form>
       </div>
     );
   }
