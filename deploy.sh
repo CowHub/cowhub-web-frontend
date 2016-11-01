ce3facd7567130516a5dde4b1a16fb17aec42971
Freddie Lindsey <freddie.a.lindsey@gmail.com>
2016-11-01 20:57:14 +0000

    No install of modules locally

--- deploy.sh
+++ deploy.sh
@@ -7,10 +7,10 @@ mkdir /app
 tar -xvf /dist.tar.gz
 mv /dist /app/dist
 mv /app-server.js /app/
+mv /node_modules /app/
 
 # Take service down
 docker rm -f $(docker ps -a -q)
 
 # Bring service up (with latest)
-docker run -v /app:/app -w /app -it node:6.9.1 npm install
 docker run -v /app:/app -p 80:8080 -w /app --name cowhub-web-frontend -dit node:6.9.1 node app-server.js
