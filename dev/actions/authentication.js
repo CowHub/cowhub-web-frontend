472eed4dff0395a22b7bf71f3af49fe72d619ddf
Freddie Lindsey <freddie.a.lindsey@gmail.com>
2017-01-03 12:11:23 +0000

    Add action for validating token

--- dev/actions/authentication.js
+++ dev/actions/authentication.js
@@ -1,84 +1,91 @@
-import $ from 'jquery';
-import store from '../store/store';
+import $ from 'jquery'
+import store from '../store/store'
 
 // Token management
-export let LOAD_TOKEN = 'LOAD_TOKEN';
-export let STORE_TOKEN = 'STORE_TOKEN';
-export let REMOVE_TOKEN = 'REMOVE_TOKEN';
+export const LOAD_TOKEN = 'LOAD_TOKEN'
+export const VALIDATE_TOKEN = 'VALIDATE_TOKEN'
+export const STORE_TOKEN = 'STORE_TOKEN'
+export const REMOVE_TOKEN = 'REMOVE_TOKEN'
 
-export function fetchToken() {
+export const fetchToken = () => {
   return {
-    type: LOAD_TOKEN,
+    type: LOAD_TOKEN
   }
 }
 
-export function storeToken(token) {
+export const validateToken = () => {
+  return {
+    type: VALIDATE_TOKEN
+  }
+}
+
+export const storeToken = (token) => {
   return {
     type: STORE_TOKEN,
-    token,
+    token
   }
 }
 
-export function removeToken() {
+export const removeToken = () => {
   return {
-    type: REMOVE_TOKEN,
+    type: REMOVE_TOKEN
   }
 }
 
 // Login
-export let LOGIN_USER_PENDING = 'LOGIN_USER_PENDING';
-export let LOGIN_USER_SUCCESS = 'LOGIN_USER_SUCCESS';
-export let LOGIN_USER_ERROR = 'LOGIN_USER_ERROR';
+export const LOGIN_USER_PENDING = 'LOGIN_USER_PENDING'
+export const LOGIN_USER_SUCCESS = 'LOGIN_USER_SUCCESS'
+export const LOGIN_USER_ERROR = 'LOGIN_USER_ERROR'
 
 // params: email, password
-export function loginUser(params) {
+export const loginUser = (params) => {
   return (dispatch) => {
-    dispatch(loginUserPending());
+    dispatch(loginUserPending())
     $.ajax(`${process.env.API_ENDPOINT}/user/authenticate`, {
       method: 'POST',
       data: params
     }).then((response) => {
-      dispatch(loginUserSuccess());
-      dispatch(storeToken(response.auth_token));
+      dispatch(loginUserSuccess())
+      dispatch(storeToken(response.auth_token))
     }).catch((error) => {
-      dispatch(loginUserError(error));
+      dispatch(loginUserError(error))
     })
   }
 }
 
-export function loginUserPending() {
+export const loginUserPending = () => {
   return {
-    type: LOGIN_USER_PENDING,
+    type: LOGIN_USER_PENDING
   }
 }
 
-export function loginUserSuccess() {
+export const loginUserSuccess = () => {
   return {
-    type: LOGIN_USER_SUCCESS,
+    type: LOGIN_USER_SUCCESS
   }
 }
 
-export function loginUserError(error) {
+export const loginUserError = (error) => {
   return {
     type: LOGIN_USER_ERROR,
-    error,
+    error
   }
 }
 
 // Register
-export let REGISTER_USER_PENDING = 'REGISTER_USER_PENDING';
-export let REGISTER_USER_SUCCESS = 'REGISTER_USER_SUCCESS';
-export let REGISTER_USER_ERROR = 'REGISTER_USER_ERROR';
+export const REGISTER_USER_PENDING = 'REGISTER_USER_PENDING'
+export const REGISTER_USER_SUCCESS = 'REGISTER_USER_SUCCESS'
+export const REGISTER_USER_ERROR = 'REGISTER_USER_ERROR'
 
 // params: email, password, password_confirmation
-export function registerUser(params) {
+export const registerUser = (params) => {
   return (dispatch) => {
-    dispatch(registerUserPending());
+    dispatch(registerUserPending())
     $.ajax(`${process.env.API_ENDPOINT}/user/create`, {
       method: 'POST',
       data: params
     }).then((response) => {
-      dispatch(registerUserSuccess());
+      dispatch(registerUserSuccess())
       dispatch(storeToken(response.auth_token))
     }).catch((error) => {
       dispatch(registerUserError(error))
@@ -86,70 +93,70 @@ export function registerUser(params) {
   }
 }
 
-export function registerUserPending() {
+export const registerUserPending = () => {
   return {
-    type: REGISTER_USER_PENDING,
+    type: REGISTER_USER_PENDING
   }
 }
 
-export function registerUserSuccess() {
+export const registerUserSuccess = () => {
   return {
-    type: REGISTER_USER_SUCCESS,
+    type: REGISTER_USER_SUCCESS
   }
 }
 
-export function registerUserError(error) {
+export const registerUserError = (error) => {
   return {
     type: REGISTER_USER_ERROR,
-    error,
+    error
   }
 }
 
 // Logout
-export let LOGOUT_USER_PENDING = 'LOGOUT_USER_PENDING';
-export let LOGOUT_USER_SUCCESS = 'LOGOUT_USER_SUCCESS';
-export let LOGOUT_USER_ERROR = 'LOGOUT_USER_ERROR';
+export const LOGOUT_USER_PENDING = 'LOGOUT_USER_PENDING'
+export const LOGOUT_USER_SUCCESS = 'LOGOUT_USER_SUCCESS'
+export const LOGOUT_USER_ERROR = 'LOGOUT_USER_ERROR'
 
-export function logoutUser() {
-  let token = store.getState().authentication.token;
+export const logoutUser = () => {
+  let token = store.getState().authentication.token
   const logoutSuccess = (dispatch) => {
-    dispatch(logoutUserSuccess());
-    dispatch(removeToken());
-  };
+    dispatch(logoutUserSuccess())
+    dispatch(removeToken())
+  }
   return (dispatch) => {
-    dispatch(logoutUserPending());
+    dispatch(logoutUserPending())
     $.ajax(`${process.env.API_ENDPOINT}/user/unauthenticate`, {
       headers: {
         'Authorization': `Bearer ${token}`
       },
       method: 'DELETE',
     }).then((response) => {
-      logoutSuccess(dispatch);
+      logoutSuccess(dispatch)
     }).catch((error) => {
       if (error.status === 401) {
-        logoutSuccess(dispatch);
+        logoutSuccess(dispatch)
       } else {
-        dispatch(logoutUserError(error));
+        dispatch(logoutUserError(error))
       }
     })
   }
 }
 
-export function logoutUserPending() {
+export const logoutUserPending = () => {
   return {
-    type: LOGOUT_USER_PENDING,
+    type: LOGOUT_USER_PENDING
   }
 }
 
-export function logoutUserSuccess() {
+export const logoutUserSuccess = () => {
   return {
-    type: LOGOUT_USER_SUCCESS,
+    type: LOGOUT_USER_SUCCESS
   }
 }
 
-export function logoutUserError(error) {
+export const logoutUserError = (error) => {
   return {
     type: LOGOUT_USER_ERROR,
-    error,
+    error
   }
 }
